9da6027a0cd43d6a3bd8b420db11b79ad7c12833
saracreates <73638285+saracreates@users.noreply.github.com>
2025-01-06 16:51:43 +0100

    Delete last blank line
    Co-authored-by: Leonhard Reichenbach <Zehvogel@users.noreply.github.com>

--- CLDConfig/py_utils.py
+++ CLDConfig/py_utils.py
@@ -228,3 +228,4 @@ def parse_collection_patch_file(patch_file: Union[str, os.PathLike]) -> List[str
 
     # Flatten the list of lists into one large list
     return [s for strings in patch_colls for s in strings]
+    
