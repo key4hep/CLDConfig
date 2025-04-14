17f01337f34da32ca537111aa41149ac1bd66b78
jmcarcell <jmcarcell@users.noreply.github.com>
2025-04-14 10:32:52 +0200

    Add DDCaloDigi to the native reconstruction chain

--- CLDConfig/CaloDigi/CaloDigi.py
+++ CLDConfig/CaloDigi/CaloDigi.py
@@ -17,117 +17,150 @@
 # limitations under the License.
 #
 from Gaudi.Configuration import WARNING
-from Configurables import MarlinProcessorWrapper
+from Configurables import MarlinProcessorWrapper, DDCaloDigi
 
 import sys
 
+ECALCollections = ["ECalBarrelCollection", "ECalEndcapCollection"]
+ECALOutputCollections = ["ECALBarrel", "ECALEndcap", ""]
+HCALCollections = ["HCalBarrelCollection", "HCalEndcapCollection", "HCalRingCollection"]
+HCALOutputCollections = ["HCALBarrel", "HCALEndcap", "HCALOther"]
+
 MyDDCaloDigiParameters = {
-                                "Histograms": ["0"],
-                                "RootFile": ["Digi_SiW.root"],
-                                "RelationOutputCollection": ["RelationCaloHit"],
-                                "energyPerEHpair": ["3.6"],
-                                # ECAL
-                                "ECALCollections": ["ECalBarrelCollection", "ECalEndcapCollection"],
-                                "ECALOutputCollection0": ["ECALBarrel"],
-                                "ECALOutputCollection1": ["ECALEndcap"],
-                                "ECALOutputCollection2": [""],
-                                "IfDigitalEcal": ["0"],
-                                "ECALLayers": ["41", "100"],
-                                "ECAL_default_layerConfig": ["000000000000000"],
-                                "StripEcal_default_nVirtualCells": ["9"],
-                                "CalibECALMIP": ["0.0001"],
-                                "ECALThreshold": ["5e-05"],
-                                "ECALThresholdUnit": ["GeV"],
-                                "ECALGapCorrection": ["1"],
-                                "ECALGapCorrectionFactor": ["1"],
-                                "ECALModuleGapCorrectionFactor": ["0.0"],
-                                "MapsEcalCorrection": ["0"],
-                                "ECAL_PPD_N_Pixels": ["10000"],
-                                "ECAL_PPD_N_Pixels_uncertainty": ["0.05"],
-                                "ECAL_PPD_PE_per_MIP": ["7"],
-                                "ECAL_apply_realistic_digi": ["0"],
-                                "ECAL_deadCellRate": ["0"],
-                                "ECAL_deadCell_memorise": ["false"],
-                                "ECAL_elec_noise_mips": ["0"],
-                                "ECAL_maxDynamicRange_MIP": ["2500"],
-                                "ECAL_miscalibration_correl": ["0"],
-                                "ECAL_miscalibration_uncorrel": ["0"],
-                                "ECAL_miscalibration_uncorrel_memorise": ["false"],
-                                "ECAL_pixel_spread": ["0.05"],
-                                "ECAL_strip_absorbtionLength": ["1e+06"],
-                                "UseEcalTiming": ["1"],
-                                "ECALCorrectTimesForPropagation": ["1"],
-                                "ECALTimeWindowMin": ["-1"],
-                                "ECALSimpleTimingCut": ["true"],
-                                "ECALDeltaTimeHitResolution": ["10"],
-                                "ECALTimeResolution": ["10"],
-                                # HCAL
-                                "HCALCollections": ["HCalBarrelCollection", "HCalEndcapCollection", "HCalRingCollection"],
-                                "HCALOutputCollection0": ["HCALBarrel"],
-                                "HCALOutputCollection1": ["HCALEndcap"],
-                                "HCALOutputCollection2": ["HCALOther"],
-                                "IfDigitalHcal": ["0"],
-                                "HCALLayers": ["100"],
-                                "CalibHCALMIP": ["0.0001"],
-                                "HCALThreshold": ["0.00025"],
-                                "HCALThresholdUnit": ["GeV"],
-                                "HCALEndcapCorrectionFactor": ["1.000"],
-                                "HCALGapCorrection": ["1"],
-                                "HCALModuleGapCorrectionFactor": ["0.5"],
-                                "HCAL_PPD_N_Pixels": ["400"],
-                                "HCAL_PPD_N_Pixels_uncertainty": ["0.05"],
-                                "HCAL_PPD_PE_per_MIP": ["10"],
-                                "HCAL_apply_realistic_digi": ["0"],
-                                "HCAL_deadCellRate": ["0"],
-                                "HCAL_deadCell_memorise": ["false"],
-                                "HCAL_elec_noise_mips": ["0"],
-                                "HCAL_maxDynamicRange_MIP": ["200"],
-                                "HCAL_miscalibration_correl": ["0"],
-                                "HCAL_miscalibration_uncorrel": ["0"],
-                                "HCAL_miscalibration_uncorrel_memorise": ["false"],
-                                "HCAL_pixel_spread": ["0"],
-                                "UseHcalTiming": ["1"],
-                                "HCALCorrectTimesForPropagation": ["1"],
-                                "HCALTimeWindowMin": ["-1"],
-                                "HCALSimpleTimingCut": ["true"],
-                                "HCALDeltaTimeHitResolution": ["10"],
-                                "HCALTimeResolution": ["10"],
+    "Histograms": 0,
+    "RootFile": "Digi_SiW.root",
+    "energyPerEHpair": 3.6,
+    # ECAL
+    "IfDigitalEcal": 0,
+    "ECALLayers": [41, 100],
+    "ECAL_default_layerConfig": "000000000000000",
+    "StripEcal_default_nVirtualCells": 9,
+    "CalibECALMIP": 0.0001,
+    "ECALThreshold": 5e-05,
+    "ECALThresholdUnit": "GeV",
+    "ECALGapCorrection": 1,
+    "ECALGapCorrectionFactor": 1,
+    "ECALModuleGapCorrectionFactor": 0.0,
+    "MapsEcalCorrection": 0,
+    "ECAL_PPD_N_Pixels": 10000,
+    "ECAL_PPD_N_Pixels_uncertainty": 0.05,
+    "ECAL_PPD_PE_per_MIP": 7,
+    "ECAL_apply_realistic_digi": 0,
+    "ECAL_deadCellRate": 0,
+    "ECAL_deadCell_memorise": False,
+    "ECAL_elec_noise_mips": 0,
+    "ECAL_maxDynamicRange_MIP": 2500,
+    "ECAL_miscalibration_correl": 0,
+    "ECAL_miscalibration_uncorrel": 0,
+    "ECAL_miscalibration_uncorrel_memorise": False,
+    "ECAL_pixel_spread": 0.05,
+    "ECAL_strip_absorbtionLength": 1e+06,
+    "UseEcalTiming": 1,
+    "ECALCorrectTimesForPropagation": 1,
+    "ECALTimeWindowMin": -1,
+    "ECALSimpleTimingCut": True,
+    "ECALDeltaTimeHitResolution": 10,
+    "ECALTimeResolution": 10,
+    # HCAL
+    "IfDigitalHcal": 0,
+    "HCALLayers": [100],
+    "CalibHCALMIP": 0.0001,
+    "HCALThreshold": [0.00025],
+    "HCALThresholdUnit": "GeV",
+    "HCALEndcapCorrectionFactor": 1.000,
+    "HCALGapCorrection": 1,
+    "HCALModuleGapCorrectionFactor": 0.5,
+    "HCAL_PPD_N_Pixels": 400,
+    "HCAL_PPD_N_Pixels_uncertainty": 0.05,
+    "HCAL_PPD_PE_per_MIP": 10,
+    "HCAL_apply_realistic_digi": 0,
+    "HCAL_deadCellRate": 0,
+    "HCAL_deadCell_memorise": False,
+    "HCAL_elec_noise_mips": 0,
+    "HCAL_maxDynamicRange_MIP": 200,
+    "HCAL_miscalibration_correl": 0,
+    "HCAL_miscalibration_uncorrel": 0,
+    "HCAL_miscalibration_uncorrel_memorise": False,
+    "HCAL_pixel_spread": 0,
+    "UseHcalTiming": 1,
+    "HCALCorrectTimesForPropagation": 1,
+    "HCALTimeWindowMin": -1,
+    "HCALSimpleTimingCut": True,
+    "HCALDeltaTimeHitResolution": 10,
+    "HCALTimeResolution": 10,
+}
+
+parameters_10ns = {
+    "CalibrECAL": [37.5227197175, 37.5227197175],
+    "ECALEndcapCorrectionFactor": 1.03245503522,
+    "ECALBarrelTimeWindowMax": 10,
+    "ECALEndcapTimeWindowMax": 10,
+    "CalibrHCALBarrel": [45.9956826061],
+    "CalibrHCALEndcap": [46.9252540291],
+    "CalibrHCALOther": [57.4588011802],
+    "HCALBarrelTimeWindowMax": 10,
+    "HCALEndcapTimeWindowMax": 10,
+}
+
+parameters_400ns = {
+    "CalibrECAL": [37.4591745147, 37.4591745147],
+    "ECALEndcapCorrectionFactor": 1.01463983425,
+    "ECALBarrelTimeWindowMax": 400,
+    "ECALEndcapTimeWindowMax": 400,
+    "CalibrHCALBarrel": [42.544403752],
+    "CalibrHCALEndcap": [42.9667604345],
+    "CalibrHCALOther": [51.3503963688],
+    "HCALBarrelTimeWindowMax": 400,
+    "HCALEndcapTimeWindowMax": 400,
 }
 
 
-MyDDCaloDigi = MarlinProcessorWrapper(f"MyDDCaloDigi_{CONFIG['CalorimeterIntegrationTimeWindow']}")
-MyDDCaloDigi.OutputLevel = WARNING
-MyDDCaloDigi.ProcessorType = "DDCaloDigi"
-MyDDCaloDigi.Parameters = MyDDCaloDigiParameters.copy()
+if reco_args.native:
+    # Not implemented
+    for key in ["Histograms", "RootFile", "ECAL_apply_realistic_digi", "HCAL_apply_realistic_digi"]:
+        MyDDCaloDigiParameters.pop(key)
+else:
+    MyDDCaloDigiParameters["RelationOutputCollection"] = "RelationCaloHit"
+    MyDDCaloDigiParameters["ECALCollections"] = ECALCollections
+    for i in range(len(ECALCollections)):
+        MyDDCaloDigiParameters[f"ECALOutputCollection{i}"] = ECALOutputCollections[i]
+    MyDDCaloDigiParameters["HCALCollections"] = HCALCollections
+    for i in range(len(HCALCollections)):
+        MyDDCaloDigiParameters[f"HCALOutputCollection{i}"] = HCALOutputCollections[i]
+
+    MyDDCaloDigi = [MarlinProcessorWrapper(f"MyDDCaloDigi_{CONFIG['CalorimeterIntegrationTimeWindow']}")]
+    MyDDCaloDigi[0].OutputLevel = WARNING
+    MyDDCaloDigi[0].ProcessorType = "DDCaloDigi"
+    MyDDCaloDigi[0].Parameters = MyDDCaloDigiParameters
+
 
 if CONFIG["CalorimeterIntegrationTimeWindow"] == "10ns":
-    MyDDCaloDigi.Parameters |= {
-                            "CalibrECAL": ["37.5227197175", "37.5227197175"],
-                            "ECALEndcapCorrectionFactor": ["1.03245503522"],
-                            "ECALBarrelTimeWindowMax": ["10"],
-                            "ECALEndcapTimeWindowMax": ["10"],
-                            "CalibrHCALBarrel": ["45.9956826061"],
-                            "CalibrHCALEndcap": ["46.9252540291"],
-                            "CalibrHCALOther": ["57.4588011802"],
-                            "HCALBarrelTimeWindowMax": ["10"],
-                            "HCALEndcapTimeWindowMax": ["10"],
-                            }
-
+    parameters = parameters_10ns
 elif CONFIG["CalorimeterIntegrationTimeWindow"] == "400ns":
-    MyDDCaloDigi.Parameters |= {
-                            "CalibrECAL": ["37.4591745147", "37.4591745147"],
-                            "ECALEndcapCorrectionFactor": ["1.01463983425"],
-                            "ECALBarrelTimeWindowMax": ["400"],
-                            "ECALEndcapTimeWindowMax": ["400"],
-                            "CalibrHCALBarrel": ["42.544403752"],
-                            "CalibrHCALEndcap": ["42.9667604345"],
-                            "CalibrHCALOther": ["51.3503963688"],
-                            "HCALBarrelTimeWindowMax": ["400"],
-                            "HCALEndcapTimeWindowMax": ["400"],
-                            }
+    parameters = parameters_400ns
 else:
     print(f"The value {CONFIG['CalorimeterIntegrationTimeWindow']} "
           "for the calorimeter integration time window is not a valid choice")
     sys.exit(1)
 
-CaloDigiSequence = [MyDDCaloDigi]
+if reco_args.native:
+    final_parameters = MyDDCaloDigiParameters | parameters
+    MyDDCaloDigi = []
+    collections = ECALCollections + HCALCollections
+    out_collections = [elem for elem in ECALOutputCollections + HCALOutputCollections if elem]
+    for incol, outcol in zip(collections, out_collections):
+        MyDDCaloDigi.append(
+            DDCaloDigi(
+                    f"{incol}Digitiser",
+                    **final_parameters,
+                    InputCaloHitCollection=[incol],
+                    OutputCaloHitCollection=[outcol],
+                    RelationOutputCollection=[f"GaudiRelationCaloHit{outcol}"],
+                    OutputLevel=WARNING,
+                    )
+            )
+else:
+    MyDDCaloDigi.Parameters |= toMarlinDict(parameters_10ns)
+
+
+CaloDigiSequence = MyDDCaloDigi
